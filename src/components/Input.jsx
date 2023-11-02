c34162f8d03226571f9884ab08e920f7abc649e9
Ahmed Saeed <96946480+AhmedSaeed217@users.noreply.github.com>
2023-11-02 04:02:18 +0200

    Delete src/components/Input.jsx

--- src/components/Input.jsx
+++
@@ -1,6 +0,0 @@
-export default function Input({ selectRadioInputs }) {
-    return <label className='sidebar-label-container'>
-        <input value={"All"} onClick={selectRadioInputs} type="radio" name="test" />
-        <span className="checkmark"></span>All
-    </label>
-}
